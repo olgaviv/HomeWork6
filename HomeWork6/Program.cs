4072b34606762c8c2ccd1899cb68f5b5c1d39454
olga <olgaviv@mail.ru>
2022-11-16 22:50:01 +0700

    first home work

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,45 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int Find(int[]array)
+{
+    int count = 0;
+    for( int i=0; i< array.Length; i++)
+        if(array[i] > 0 )
+            count++;
+    return count;
+}
+
+int [] CreateArrayPols(int size)
+{
+    int[] array = new int[size];
+
+    int i = 0;
+       while (i < size)
+        {
+           Console.WriteLine("Введите элемент массива");
+           array[i] = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine();
+           i++;
+        }
+    
+    return array;
+}
+
+        int m;
+        Console.WriteLine("Введите число элементов массива");
+        m = Convert.ToInt16(Console.ReadLine());
+        
+int[] myArray = CreateArrayPols(m);
+ShowArray(myArray);
+Console.WriteLine($"Количество чисел больше нуля  = {Find(myArray)}");
+
+*/
+
+// 
